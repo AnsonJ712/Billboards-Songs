4a501d7117fee38f6df3176090face95d44a7cf6
ans0nJ <ans0nJ72@gmail.com>
2018-03-19 22:14:19 -0700

    Added classes

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export SPOTIPY_CLIENT_ID='fa7711b32c9047849df65cfd7ce8c634'
+export SPOTIPY_CLIENT_SECRET='6c45a93d8d3e496181d1f37cb6b3d98d'
+export SPOTIPY_REDIRECT_URI='http://localhost'
+
+python Billboards.py 22lowm2thyomlyic63iohy52a
