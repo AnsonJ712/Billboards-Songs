8e4ef36e710bfa9dd65d06ca0832696aaa0d299b
ans0nJ <ans0nJ72@gmail.com>
2018-06-04 15:18:22 -0700

    Added more options

--- create_playlist.py
+++ create_playlist.py
@@ -14,22 +14,30 @@ def main():
 
     if sys.argv[2] == "Hip-Hop":
         link = "https://www.billboard.com/charts/r-b-hip-hop-songs"
-        playlist_name = "Billboard R&B/Hip-Hop Songs"
-    elif sys.argv[2] == "Dance":
+    elif sys.argv[2] == "Dance/Electronic":
         link = "https://www.billboard.com/charts/dance-electronic-songs"
-        playlist_name = "Billboard Dance/Electronic Songs"
+    elif sys.argv[2] == "Country":
+        link = "https://www.billboard.com/charts/country-songs"
+    elif sys.argv[2] == "Rock":
+        link = "https://www.billboard.com/charts/rock-songs"
+    elif sys.argv[2] == "Latin":
+        link = "https://www.billboard.com/charts/latin-songs"
+    elif sys.argv[2] == "Pop":
+        link = "https://www.billboard.com/charts/pop-songs"
+
+    playlist_name = "Billboard " + sys.argv[2] + " Songs"
 
     scope = 'playlist-modify-private playlist-modify'
 
     charts = Billboard(link)
-    page = charts.get_page(link)
+    page = charts.get_page()
     soup = charts.parse_page(page)
     artists = charts.get_tracks_from_billboard(soup)[0]
     songs = charts.get_tracks_from_billboard(soup)[1]
 
     spotify = Spotify(scope)
-    user = spotify.authenticate(scope, sys.argv[1])[0]
-    sp = spotify.authenticate(scope, sys.argv[1])[1]
+    user = spotify.authenticate(sys.argv[1])[0]
+    sp = spotify.authenticate(sys.argv[1])[1]
     spotify.add_to_playlist(sp, user, artists, songs, playlist_name)
 
     print("Done!")
