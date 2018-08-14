cca908ecba12c5954b3502414435a0106a63db35
ans0nJ <ans0nJ72@gmail.com>
2018-08-14 14:03:31 -0700

    Changed class names

---
+++ billboard.py
@@ -0,0 +1,24 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+
+class Billboard:
+    def __init__(self, link):
+        self.link = link
+
+    def get_page(self):
+        return requests.get(self.link)
+
+    def parse_page(self, page):
+        return BeautifulSoup(page.content, 'html.parser')
+
+    def get_tracks_from_billboard(self, soup):
+        """
+        Collects song titles from billboards.com
+        """
+        chart_name = soup.find(class_='chart-detail-header__chart-name')
+        artists = soup.find_all(class_=['chart-number-one__artist', 'chart-list-item__artist'])
+        songs = soup.find_all(class_=['chart-number-one__title', 'chart-list-item__title-text'])
+        
+        return artists, songs, chart_name
